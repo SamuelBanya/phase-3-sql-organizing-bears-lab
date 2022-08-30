4b7953f526717e84bb0e1bd67b414b24cfc4af24
Samuel Banya <sbanya@fastmail.com>
2022-08-29 20:28:37 -0400

    Adding latest changes to assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 5, "M", "Black", "Well Mannered", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 2, "M", "White", "Well Mannered", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 3, "M", "Black", "Well Mannered", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 1, "M", "White", "Well Mannered", 1);
+INSERT INTO bears VALUES (5, "Melissa", 8, "M", "Orange", "Bad", 1);
+INSERT INTO bears VALUES (6, "Grinch", 10, "M", "Orange", "Bad", 1);
+INSERT INTO bears VALUES (7, "Wendy", 15, "M", "Orange", "Bad", 1);
+INSERT INTO bears VALUES (8, NULL, 25, "M", "Black", "Well Mannered", 1);
